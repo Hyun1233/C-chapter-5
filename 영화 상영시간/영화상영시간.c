6eab36dd724cbe2d51cd1b762b7ccfe06af4910f
// <//>
2023-04-11 22:42:35 +0900

    초기 커밋.

---
+++ 영화 상영시간/영화상영시간.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void convert_time(int minutes);
+
+int main(void) {
+	int time = 118; // 총 시간(분 단위)
+	convert_time(time); // 함수 호출
+	return 0;
+}
+
+void convert_time(int minutes) { //함수 정의
+	int hour = minutes / 60; // 시간 : 총 시간을 60으로 나눈 값
+	int min = minutes % 60; // 분 : 총 시간을 60으로 나눈 나머지
+	printf("%d시간 %d분\n", hour, min); // 결과 출력
+}
